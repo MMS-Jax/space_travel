4b69a1eb0de7c2cfe9294e301455581f723671a2
Ryan Kelley <kelleyr@duvalschools.org>
2018-02-02 12:04:08 -0500

    First Version
    This is the first version of the program.  It serves as a starting point.
    
    The first thing you should do is declare all of the important variables the program will use.

---
+++ kelley_ryan_space_travel.js
@@ -0,0 +1,11 @@
+/*
+Space Travel Calculator by Ryan K. 
+Version 0.0a 
+February 02, 2018
+*/ 
+
+// Declare important const variables first. 
+
+const distanceToMars = 1234567; // You need to look up the ACTUAL DISTANCE IN KILOMETERS.  
+const distanceToSun = 7654321;
+// Add all of your variables before continuining. 
