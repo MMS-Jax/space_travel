dbe9cbccb751fd8ee70d5a9085703e24f182ebdd
Ryan Kelley <kelleyr@duvalschools.org>
2018-02-06 11:45:29 -0500

    Variables Declared
    This version has all of my const variables declared.  Values still need to be assigned for each though.

--- kelley_ryan_space_travel.js
+++ kelley_ryan_space_travel.js
@@ -4,8 +4,15 @@ Version 0.0a
 February 02, 2018
 */ 
 
-// Declare important const variables first. 
+// Declare important const variables first.  Distances measured in millions of kilometers. 
 
-const distanceToMars = 1234567; // You need to look up the ACTUAL DISTANCE IN KILOMETERS.  
+const distanceToMars = 1234567; // You need to enter the actual distance values here.  Do not use commas.  
 const distanceToSun = 7654321;
+const distanceToPluto = ; 
+const distanceToJupiter; 
+const distanceToNeptune; 
+
+// Distances for objects outside the Solar System. 
+
+
 // Add all of your variables before continuining. 
---
+++ kelley_ryan_space_travel0.1a.js
@@ -0,0 +1,23 @@
+/*
+Space Travel Calculator by Ryan K. 
+Version 0.1a 
+February 06, 2018
+*/ 
+
+// Declare important const variables first.  Distances measured in millions of kilometers. 
+
+const distanceToMars = 1234567; // You need to enter the actual distance values here.  Do not use commas.  
+const distanceToSun; // This line declares the variable but does not assign a value yet.  Must finish later. 
+const distanceToPluto; // Please note that variable names follow camelCase rules. 
+const distanceToJupiter; 
+const distanceToNeptune; 
+
+// Distances for objects outside the Solar System. Also measured in millions of kilometers. 
+
+const distanceToAlphaCentauri;
+const distanceToCrabNebula;
+const distanceToProximaCentauri;
+const distanceToEagleNebula;
+const distanceToPillarsOfCreation; 
+
+// Add all of your variables before continuining. 
