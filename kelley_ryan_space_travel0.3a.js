fb3910cd9cf5fc6ea8c53a54dfbfe90b5a740c36
Ryan Kelley <kelleyr@duvalschools.org>
2018-02-07 11:30:33 -0500

    User Input
    Added options for collecting the user input choice for which object to calculate.

---
+++ kelley_ryan_space_travel0.3a.js
@@ -0,0 +1,51 @@
+/*
+Space Travel Calculator by Ryan K. 
+Version 0.3a 
+February 07, 2018
+*/ 
+
+// Declare important const variables first.  Distances measured in millions of kilometers. 
+
+const distanceToMars = 1; // You need to enter the actual distance values here.  Do not use commas.  
+const distanceToSun = 1; // Please note that variable names follow camelCase rules. 
+const distanceToPluto = 1; 
+const distanceToJupiter = 1; 
+const distanceToNeptune = 1; 
+
+// Distances for objects outside the Solar System. Also measured in millions of kilometers. 
+
+const distanceToAlphaCentauri = 1;
+const distanceToCrabNebula = 1;
+const distanceToProximaCentauri = 1;
+const distanceToEagleNebula = 1;
+const distanceToPillarsOfCreation = 1; 
+
+// Let's explain what the program does to the user.  You need to change this to your own words.  
+
+console.log("Welcome to the Space Travel Calculator!");
+console.log("NASA has researched a number of objects inside and outside of the Solar System.");
+console.log("This program will help calculate the amount of travel time to each of the objects.");
+console.log("You will need to select one of the objects from the list."); 
+
+/*
+ Use console.log() to provide a list of options to the user.  One example is provided for you.  The user will select their destination by typing in 
+ the number from the list.   
+*/  
+
+console.log("[0] Mars");
+
+
+/* 
+Next we need to have the user select a destination and save that data to a variable. 
+Which of the following methods would work best?
+    
+    var user_choice = input("Type the number of the destination and press the [Enter] key.");
+    var user_choice = read_data("Type the number of the destination and press the [Enter] key.");
+    var user_choice = data_read("Type the number of the destination and press the [Enter] key.");
+    var user_choice = prompt("Type the number of the destination and press the [Enter] key.");
+
+Use your Internet research skills to determine which of the above lines of code would work best.     
+*/ 
+
+// REPLACE THIS LINE WITH THE CODE YOU WANT TO USE TO STORE THE USER'S CHOICE.
+
