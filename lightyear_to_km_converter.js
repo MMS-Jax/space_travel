0b0538d692adbff7b3e4483cda983192cfbcc47c
Ryan Kelley <kelleyr@duvalschools.org>
2018-02-08 12:43:26 -0500

    LightYear to Km Converter
    This is an example to convert Lightyears to kilometers.

---
+++ lightyear_to_km_converter.js
@@ -0,0 +1,10 @@
+// Quick and easy LightYear/AU Converter.  
+
+
+// This variable tells the program how many Km in a Light Year. 
+
+const kilometersPerLightYear = 12345678;  // YOU NEED TO FIND THE ACTUAL VALUE! 
+
+// Distances in Light Years. 
+
+const distanceToUranus = 5.55 * kilometersPerLightYear; 
