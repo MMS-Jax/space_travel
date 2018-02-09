7d371699f3f8a1b369defb6001ba8f0cc16ea666
Ryan Kelley <kelleyr@duvalschools.org>
2018-02-09 12:06:22 -0500

    Updated Code
    Re-wrote code to make it easier to understand.

--- kelley_ryan_space_travel0.5a.js
+++ kelley_ryan_space_travel0.5a.js
@@ -1,27 +1,29 @@
 /*
 Space Travel Calculator by Ryan K. 
-Version 0.4a 
-February 07, 2018
+Version 0.5a 
+February 09, 2018
 */ 
 
-// Declare important const variables first.  Distances measured in millions of kilometers. 
-
-const distanceToMars = 1; // You need to enter the actual distance values here.  Do not use commas.  
-const distanceToSun = 1; // Please note that variable names follow camelCase rules. 
-const distanceToPluto = 1; 
-const distanceToJupiter = 1; 
-const distanceToNeptune = 1; 
-
-// Distances for objects outside the Solar System. Also measured in millions of kilometers. 
-
-const distanceToAlphaCentauri = 1;
-const distanceToCrabNebula = 1;
-const distanceToProximaCentauri = 1;
-const distanceToEagleNebula = 1;
-const distanceToPillarsOfCreation = 1; 
+// Declare million and billion as variables. 
+const million = 1000000;
+const billion = 1000000000;
+const trillion = 1000000000000;
+/*
+Distances measured in kilometers. 
+Please note that variable names follow camelCase rules. 
+*/ 
+const distanceToMars = 54.6 * million;  
+const distanceToSun = 92.96 * million; 
+const distanceToJupiter = 588 * million; 
+const distanceToPluto = 7.5 * billion; 
+const distanceToNeptune = 2.7 * billion; 
+const distanceToAlphaCentauri = 41.32 * trillion;
+const distanceToProximaCentauri = 40.14 * trillion;
+const distanceToCrabNebula = 6.171e16;
+const distanceToEagleNebula = 6.622e16;
+const distanceToPillarsOfCreation = 6.6225e16; 
 
 // Let's explain what the program does to the user.  You need to change this to your own words.  
-
 console.log("Welcome to the Space Travel Calculator!");
 console.log("NASA has researched a number of objects inside and outside of the Solar System.");
 console.log("This program will help calculate the amount of travel time to each of the objects.");
@@ -31,57 +33,54 @@ console.log("You will need to select one of the objects from the list.");
  Use console.log() to provide a list of options to the user.  One example is provided for you.  The user will select their destination by typing in 
  the number from the list.   
 */  
-
 console.log("[0] Mars");
+console.log("[1] Sun");
+console.log("[2] Jupiter");
+console.log("[3] Pluto");
+console.log("[4] Neptune");
+console.log("[5] Alpha Centauri");
+console.log("[6] Proxima Centauri");
+console.log("[7] The Crab Nebula");
+console.log("[8] Eagle Nebula");
+console.log("[9] The Pillars of Creation");
 
+// Ask the user where they want to go. Save that value as a variable named user_choice.
+var user_choice = prompt("Type the number of the destination from the list and press the [Enter] key.");
 
-/* 
-Next we need to have the user select a destination and save that data to a variable. 
-Which of the following methods would work best?
-    
-    var user_choice = input("Type the number of the destination and press the [Enter] key.");
-    var user_choice = read_data("Type the number of the destination and press the [Enter] key.");
-    var user_choice = data_read("Type the number of the destination and press the [Enter] key.");
-    var user_choice = prompt("Type the number of the destination and press the [Enter] key.");
-
-Use your Internet research skills to determine which of the above lines of code would work best.     
-*/ 
-
-// REPLACE THIS LINE WITH THE CODE YOU WANT TO USE TO STORE THE USER'S CHOICE.
-
-/*
-Now that we have saved the user choice into a variable we need to figure out what to do with it.  There are at least two programming structures we can
-use to accomplish this. 
-
-One is using an if/else statement to determine which distance to use. 
-
-    if (user_choice == 1) {
-        console.log("You have chosen Mars.");
-        var distance = distanceToMars;
-    } else if (user_choice == 2) {
-        console.log("You have chosen option 2.");
-        var distance = distanceToOption2;
-    } else {
-        console.log("You did not choose a viable option.  The program will now close.");
-    }
-
-Another method is using a switch statement.  
-
-    switch(user_choice) {
-        case "1":
-            console.log("You have chosen Mars.");
-            var distance = distanceToMars;
-            break;
-        case "2":
-            console.log("You have chosen option 2.");
-            var distance = distanceToOption2;
-            break;
-        default:
-            console.log("You did not choose a viable option.  The program will now close.");
-    }
-
-Again, use your Internet research skills to determine which of the above you would like to use. 
-*/ 
+// Next we need to determine which desination was chosen. 
+if (user_choice == 0) {
+    console.log("You have chosen Mars.");
+    var distance = distanceToMars;
+} else if (user_choice == 1) {
+    console.log("You have chosen The Sun");
+    var distance = distanceToSun;
+} else if (user_choice == 2) {
+    console.log("You have chosen Jupiter.");
+    var distance = distanceToJupiter;
+} else if (user_choice == 3) {
+    console.log("You have chosen Pluto");
+    var distance = distanceToPluto;
+} else if (user_choice == 4) {
+    console.log("You have chosen Neptune.");
+    var distance = distanceToNeptune;
+} else if (user_choice == 5) {
+    console.log("You have chosen Alpha Centauri.");
+    var distance = distanceToAlphaCentauri;
+} else if (user_choice == 6) {
+    console.log("You have chosen Proxima Centauri");
+    var distance = distanceToProximaCentauri;
+} else if (user_choice == 7) {
+    console.log("You have chosen The Crab Nebula");
+    var distance = distanceToCrabNebula;
+} else if (user_choice == 8) {
+    console.log("You have chosen The Eagle Nebula.");
+    var distance = distanceToEagleNebula;
+} else if (user_choice == 9) {
+    console.log("You have chosen The Pillars of Creation.");
+    var distance = distanceToPillarsOfCreation;
+} else {
+    console.log("You did not choose a viable option.  Please try again.");
+}
 
 /*  
 Now that you have determined the destination, we need to figure out how fast you will travel. 
