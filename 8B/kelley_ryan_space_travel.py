bf3c033fe58c4dfccb2b6a5fdc06870bdfd9e160
Ryan K <kelleyr@duvalschools.org>
2020-09-21 14:52:04 -0400

    Second Commit
    Added comments with program requirements.

--- 8B/kelley_ryan_space_travel.py
+++ 8B/kelley_ryan_space_travel.py
@@ -1,1 +1,8 @@
 # Space Travel Simulator, Ryan Kelley, September 21, 2020 2:29PM, Version 0.0
+
+# print instructions for the user
+# get user input for selection
+# calculate distance to selection
+# get user input for speed, check to make sure it does not exceed light speed
+# calculate travel time
+
