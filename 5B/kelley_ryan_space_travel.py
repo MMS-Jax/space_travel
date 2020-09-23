340bcfb3b0fc3c42d0ac2b9cbf2fc4dccbf533a9
Ryan Kelley <kelleyr@duvalschools.org>
2020-09-23 09:19:04 -0400

    Version 0.1
    Added print() method with brief instructions.

--- 5B/kelley_ryan_space_travel.py
+++ 5B/kelley_ryan_space_travel.py
@@ -1,4 +1,4 @@
-# Space Travel Simulator 2020, Ryan Kelley, 09/21/2020 9:24AM, Version 0.0
+# Space Travel Simulator 2020, Ryan Kelley, 09/23/2020 9:16AM, Version 0.1
 
 # print instructions on the screen
 # get user input to select an object
@@ -12,3 +12,6 @@
 
 # ALGORITHM - a step-by-step process to solve a problem / complete a task.  
  
+print ("Hello!  Thank you for using the NASA Space Travel Simulator 2020!  This program will help you travel through space.")
+# Line 15 is a STATEMENT.  A statement is one complete line of code.  
+# print is a METHOD.  Methods are basically "special abilities" built in to the Python code. 
