3b5f046b15b017fa3e086db323a309aee6350c4e
Ryan Kelley <kelleyr@duvalschools.org>
2020-09-23 14:00:12 -0400

    Version 0.1
    Added print() method to display a thank you message and a quick intro.

--- 8B/kelley_ryan_space_travel.py
+++ 8B/kelley_ryan_space_travel.py
@@ -1,8 +1,13 @@
-# Space Travel Simulator, Ryan Kelley, September 21, 2020 2:29PM, Version 0.0
+# Space Travel Simulator, Ryan Kelley, September 23, 2020 1:58PM, Version 0.1
 
 # print instructions for the user
 # get user input for selection
 # calculate distance to selection
 # get user input for speed, check to make sure it does not exceed light speed
 # calculate travel time
+
+print ("Thank you for using the NASA Space Travel Simulator!  This program will determine whether you can safely travel in outer space.") 
+# Line 9 is a STATEMENT.  A statement is a complete line of code.
+# print is a METHOD.  A method is a "special ability" built in to Python that programmers can use.  
+
 
