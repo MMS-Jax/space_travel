9dcac44f1a6edfda2273a062d746845287215d4d
Ryan Kelley <kelleyr@duvalschools.org>
2020-09-23 08:58:40 -0400

    Added Steps for Algorithm
    Brainstormed ideas for steps to solve the space travel problem.
    
    Added definition of ALGORITHM.

--- 5B/kelley_ryan_space_travel.py
+++ 5B/kelley_ryan_space_travel.py
@@ -1,1 +1,14 @@
 # Space Travel Simulator 2020, Ryan Kelley, 09/21/2020 9:24AM, Version 0.0
+
+# print instructions on the screen
+# get user input to select an object
+# determine distance to the object
+# get user input for speed 
+# make sure it's not faster than speed of light
+# calculate travel time
+# determine if travel time is greater than 3 years.
+# if more than three years, PRINT WARNING OF GRAVE DANGER OR DEATH
+# if less than or equal to three years, PRINT HAPPY MESSAGE OF GOOD LUCK
+
+# ALGORITHM - a step-by-step process to solve a problem / complete a task.  
+ 
