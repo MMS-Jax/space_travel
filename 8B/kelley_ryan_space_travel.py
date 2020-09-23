6e1972489cdfd834281d6c59fab8e2c1a422471f
Ryan Kelley <kelleyr@duvalschools.org>
2020-09-23 14:30:37 -0400

    Version 0.2
    Declared light_speed as a variable, intiatilzed it to 299,792.
    
    printed the speed of light to screen for the user.

--- 8B/kelley_ryan_space_travel.py
+++ 8B/kelley_ryan_space_travel.py
@@ -1,4 +1,4 @@
-# Space Travel Simulator, Ryan Kelley, September 23, 2020 1:58PM, Version 0.1
+# Space Travel Simulator, Ryan Kelley, September 23, 2020 2:26PM, Version 0.2
 
 # print instructions for the user
 # get user input for selection
@@ -11,3 +11,14 @@ print ("Thank you for using the NASA Space Travel Simulator!  This program will 
 # print is a METHOD.  A method is a "special ability" built in to Python that programmers can use.  
 
 
+# DECLARING VARIABLES
+# Variables allow us to store data.
+# The main types of INTEGERS (int), DECIMALS (floats), and STRINGS (string).
+# Variable names can be camelCase or snake_case.
+# In Python, variable names can start with a letter or an underscore but NOT a number.
+# game_1, num_eggs, amnt_gas, or _favorite_flavor are all LEGAL variable names in Python.
+
+light_speed = 299792
+# DECLARED light_speed as a variable.  The = sign INITIALIZES the variable, which means giving it a starting value.
+print("The speed of light is", light_speed, "Km / s.") 
+# When you use a variable_name inside of a string it is called SUBSTITUTION. 
