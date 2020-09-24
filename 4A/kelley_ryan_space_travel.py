cf8c64c90acef08e4a0de0c22755bcd4621a04cd
Ryan K <kelleyr@duvalschools.org>
2020-09-24 14:30:36 -0400

    Second Commit
    added the steps we need to solve the problem.

--- 4A/kelley_ryan_space_travel.py
+++ 4A/kelley_ryan_space_travel.py
@@ -1,1 +1,11 @@
 # Space Travel Simulator, Ryan Kelley, Sept. 24, 2020, 1:53PM, Version 0.0
+
+# print instructions
+# get user selection
+# calculate the distance to the object
+# ask how fast you are traveling
+# calculate how long it will take (Distance / Speed)
+# Check to see if the trip takes more or less than three years.
+# If the trip is MORE THAN 3 years, print a warning
+# If the trip is LESS THAN OR EQUAL TO 3 years, print a message of success.
+
