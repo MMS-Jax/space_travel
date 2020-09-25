a26508255b6d6e83b5fe1df538e83ebd122596b6
Ryan K <kelleyr@duvalschools.org>
2020-09-25 13:15:51 -0400

    Version 0.05
    Broke down the ALGORITH for solving our problem.

--- 2020-21/7B/kelley_ryan_space_travel.py
+++ 2020-21/7B/kelley_ryan_space_travel.py
@@ -1,1 +1,14 @@
-# Space Travel Simulator, Ryan Kelley, Sept. 23 2020 1:04PM, Version 0.0 
+# Space Travel Simulator, Ryan Kelley, Sept. 25 2020 1:13PM, Version 0.05
+# ALGORITHM -- step-by-step list of instructions to complete a task.
+
+# Give the user instructions about what the program does / how to use it.
+# Ask the user for their name.
+# Present a menu of different places in outer space to travel to.
+# Allow the user to pick a place from the menu.
+# Calculate the distance to the object.
+# Ask the user how fast they want to go.
+# Make sure it's not faster than the speed of light.
+# Calculate TIME by Distance / Speed (/ means DIVIDE in programming)
+# Determine if trip is MORE OR LESS than 3 years. 
+
+
