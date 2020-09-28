30471b094892b3c1b23ca87ca6ad649f875c6bf3
Ryan K <kelleyr@duvalschools.org>
2020-09-28 13:18:42 -0400

    [3A] Version 0.1
    Added print() with instructions.

--- 2020-21/3A/kelley_ryan_space_travel.py
+++ 2020-21/3A/kelley_ryan_space_travel.py
@@ -1,1 +1,14 @@
-# Space Travel Simulator, Ryan Kelley, 09/24/20 12:50PM, Version 0.0
+# Space Travel Simulator, Ryan Kelley, 09/28/20 1:16PM, Version 0.1
+# Lines that start with a # are known as COMMENTS.  Python ignores them.
+# ALGORITHM -- List of step-by-step instructions to solve a problem / complete a task. 
+
+# Print out a greeting and instructions.
+# Provide a menu of places in outer space to travel to.
+# Allow the user to select a place.
+# Calculate the distance to that place.
+# Ask the user how fast they are going.
+# Determine how MUCH TIME it will take to get there.
+# Determine if that is more than three years.
+
+
+print ("Welcome to the NASA Space Travel Simulator!  This program will help you travel in space.")
