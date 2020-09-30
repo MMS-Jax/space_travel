675449bb6e9a281dc580a1046a5a0d1638bb8048
Ryan K <kelleyr@duvalschools.org>
2020-09-30 12:56:12 -0400

    [3A] Version 0.15
    Added import time, and comment explaining what a METHOD is.

--- 2020-21/3A/kelley_ryan_space_travel.py
+++ 2020-21/3A/kelley_ryan_space_travel.py
@@ -1,6 +1,7 @@
-# Space Travel Simulator, Ryan Kelley, 09/28/20 1:16PM, Version 0.1
+# Space Travel Simulator, Ryan Kelley, 09/30/20 12:53PM, Version 0.15
 # ALGORITHM -- List of step-by-step instructions to solve a problem / complete a task. 
 
+import time 
 # Print out a greeting and instructions.
 # Provide a menu of places in outer space to travel to.
 # Allow the user to select a place.
@@ -9,4 +10,6 @@
 # Determine how MUCH TIME it will take to get there.
 # Determine if that is more than three years.
 
+
 print ("Welcome to the NASA Space Travel Simulator!  This program will help you travel in space.")
+# print() is known as a METHOD.  It is a built-in "ability" for Python programmers to use. 
