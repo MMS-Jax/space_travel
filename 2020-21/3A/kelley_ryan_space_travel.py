e625e9386dfe1d715d2643d2e77b202baf714780
Ryan K <kelleyr@duvalschools.org>
2020-09-30 13:09:20 -0400

    [3A] Version 0.25
    Added comments about naming variables.

--- 2020-21/3A/kelley_ryan_space_travel.py
+++ 2020-21/3A/kelley_ryan_space_travel.py
@@ -1,4 +1,4 @@
-# Space Travel Simulator, Ryan Kelley, 09/30/20 1:01PM, Version 0.2
+# Space Travel Simulator, Ryan Kelley, 09/30/20 1:08PM, Version 0.25
 # ALGORITHM -- List of step-by-step instructions to solve a problem / complete a task. 
 
 import time 
@@ -22,5 +22,11 @@ print ("Welcome to the NASA Space Travel Simulator!  This program will help you 
 # “Floats (floating point) are positive or negative numbers that have a decimal. Abbreviated as float.”
 # “Strings are lines of text including letters and characters. Abbreviated as str.”
 # “Bools (Boolean) are True of False values. Abbreviated as bool.”
+
+# NAMING VARIABLES
+# “Variables should ALWAYS be descriptive. Should be able to identify what type of data is stored in the variable.”
+# “Examples include: num_eggs, amnt_gas, high_score, or player_name.”
+# “Variables in Python can start with _ or a letter but NOT a number.”
+# “Variables can use camelCase style or snake_case style but use the SAME style in your code.”
 
 
