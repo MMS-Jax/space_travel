51ee2714bdb163f483dcc547aaa2a43e9b979237
Ryan K <kelleyr@duvalschools.org>
2020-09-30 13:05:45 -0400

    [3A] Version 0.2
    Added comments with information about variables.

--- 2020-21/3A/kelley_ryan_space_travel.py
+++ 2020-21/3A/kelley_ryan_space_travel.py
@@ -1,4 +1,4 @@
-# Space Travel Simulator, Ryan Kelley, 09/30/20 12:53PM, Version 0.15
+# Space Travel Simulator, Ryan Kelley, 09/30/20 1:01PM, Version 0.2
 # ALGORITHM -- List of step-by-step instructions to solve a problem / complete a task. 
 
 import time 
@@ -12,4 +12,15 @@ import time
 
 
 print ("Welcome to the NASA Space Travel Simulator!  This program will help you travel in space.")
-# print() is known as a METHOD.  It is a built-in "ability" for Python programmers to use. 
+# print() is known as a METHOD.  It is a built-in "ability" for Python programmers to use.
+
+
+# DECLARING VARIABLES
+# “Variables store different types of data.”
+# Most Common Data Types are: INTEGERS, FLOATS, STRINGS, and BOOLS.”
+# “Integers are positive or negative whole numbers, including zero. Abbreviated as int.”
+# “Floats (floating point) are positive or negative numbers that have a decimal. Abbreviated as float.”
+# “Strings are lines of text including letters and characters. Abbreviated as str.”
+# “Bools (Boolean) are True of False values. Abbreviated as bool.”
+
+
